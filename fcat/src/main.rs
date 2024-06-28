6fa363817f2b8f8ee40a21b062321689a332e9c2
miguevrgo <miguevrgo@gmail.com>
2024-06-28 15:29:05 +0200

    Upload cat in Rust, TODO: Numbers, Box, Movement

--- fcat/src/main.rs
+++ fcat/src/main.rs
@@ -1,5 +1,6 @@
 use std::env;
-use std::io::{self, BufRead, BufReader, Write};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 use text_colorizer::*;
 
 fn print_usage() {
@@ -16,6 +17,24 @@ fn main() {
         eprintln!("{} wrong number of arguments: expected > 1, got {}.",
                   "Error:".red().bold(), args.len());
         std::process::exit(1);
+    }
+
+    for file_name in &args[1..] {
+        match File::open(file_name) {
+            Ok(file) => {
+                let reader = BufReader::new(file);
+
+                for line in reader.lines() {
+                    match line {
+                        Ok(line_content) => println!("{}", line_content),
+                        Err(e) => eprintln!("Error reading {}: {}", file_name, e)
+                    }
+                }
+            }
+            Err(e) => {
+                eprintln!("{} opening {}: {}", "Error".red().bold(), file_name, e);
+            }
+        }
     }
 
 
