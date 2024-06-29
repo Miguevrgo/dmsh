f99b3935dc535860411de203a0fa561bf6ad115c
Miguevrgo <Miguevrgo@gmail.com>
2024-06-29 23:24:15 +0200

    Upload qcat style idea, TODO: Refactor name

--- builtin/fcat/src/main.rs
+++ builtin/fcat/src/main.rs
@@ -4,18 +4,46 @@ use std::io::{BufRead, BufReader};
 use text_colorizer::*;
 
 fn print_usage() {
-    eprintln!("{} - concatenate files and print on the standard output",
-              "qcat".green());
+    eprintln!(
+        "{} - concatenate files and print on the standard output",
+        "qcat".green()
+    );
     eprintln!("Usage: qcat [OPTION]... [FILE]...");
 }
 
+/// TODO: Change this behaviour, uploaded for contributors to know the idea
+/// behind, missing adjusting numbers and lines, maybe changing filename to exclude
+/// route
+fn print_boxed_content(file_name: &str, lines: Vec<String>) {
+    let max_line_width = lines.iter().map(|line| line.len()).max().unwrap_or(0);
+    let width = max_line_width + 4;
+    let max_width = width;
+
+    println!("┏{:━<width$}┓", "", width = width);
+    println!("┃ {: ^width$} ┃", file_name.green(), width = width - 2);
+    println!("┗{:━<width$}┛", "", width = width);
+
+    for (index, line) in lines.iter().enumerate() {
+        println!(
+            "┃ {: >4} ┃ {: ^max_width$} ┃",
+            index + 1,
+            line,
+            max_width = max_width
+        );
+    }
+
+    println!("┗{:━<width$}┛", "", width = width);
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         print_usage();
-        eprintln!("{} wrong number of arguments: expected > 1, got {}.",
-                  "Error:".red().bold(), args.len());
+        eprintln!(
+            "{} wrong number of arguments: expected > 1, got {}.",
+            "Error:".red().bold(),
+            args.len()
+        );
         std::process::exit(1);
     }
 
@@ -23,20 +51,17 @@ fn main() {
         match File::open(file_name) {
             Ok(file) => {
                 let reader = BufReader::new(file);
-
-                for line in reader.lines() {
-                    match line {
-                        Ok(line_content) => println!("{}", line_content),
-                        Err(e) => eprintln!("Error reading {}: {}", file_name, e)
-                    }
-                }
+                let lines: Vec<String> = reader
+                    .lines()
+                    .map(|l| l.unwrap_or_else(|_| String::new()))
+                    .collect();
+                print_boxed_content(&file_name, lines);
             }
             Err(e) => {
-                eprintln!("{} opening {}: {}", "Error".red().bold(), file_name, e);
+                eprintln!("Error opening {}: {}", file_name, e);
+                std::process::exit(1);
             }
         }
     }
-
-
-
 }
+
