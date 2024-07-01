55c3b72f420cee476be2c3b9fbc0c1327fa67cfc
Daniel G <gmzdaniel@protonmail.com>
2024-07-01 18:33:37 +0200

    Finish implementing REPL
    TODO: handle exit gracefully (i.e. don't try to fork-exec `exit`).

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define STR(x) #x
 #define DMSH_TOKEN_EXIT STR(exit)
@@ -15,6 +18,12 @@
 	} while(0)
 #define DMSH_TOKEN_BUFSIZE 64
 #define DMSH_TOKEN_DELIM " \t\r\n\a"
+#define DMSH_PERRNEXIT(str) \
+	do { \
+		perror("dmsh"); \
+		exit(EXIT_FAILURE); \
+	} while(0)
+#define DMSH_NO_STATUS 256 /* Maximum exit value for process is 255 */
 
 int
 dmsh_continue(const char *line)
@@ -90,16 +99,48 @@ dmsh_split_line(char *line)
 }
 
 int
+dmsh_launch(char **args)
+{
+	pid_t pid, wpid;
+	int status, ret;
+
+	pid = fork();
+	if (pid == 0) {
+		/* I'm the child */
+		if (execvp(args[0], args) < 0) {
+			DMSH_PERRNEXIT("dmsh");
+		}
+	} else if (pid < 0) {
+		DMSH_PERRNEXIT("dmsh");
+	} else {
+		/* I'm the parent and my child's ok */
+		if ((wpid = wait(&status)) < 0) {
+			DMSH_PERRNEXIT("dmsh");
+		}
+		if (WIFEXITED(status)) {
+			/* Child called _exit, exit, or return from main */
+			ret = WEXITSTATUS(status);
+		} else {
+			ret = DMSH_NO_STATUS;
+		}
+	}
+
+	return ret;
+}
+
+int
 main(void)
 {
 	char *line;
 	char **tokens;
+	int ret = 0;
 
 	printf("Type \"exit\" to exit the shell\n");
 	do {
-		printf(DMSH_PROMPT);
+		printf("(%d) %s", ret, DMSH_PROMPT);
 		line = dmsh_read_line();
 		tokens = dmsh_split_line(line);
+		ret = dmsh_launch(tokens);
 	} while (free(tokens), dmsh_continue_and_free(line));
 
 	return EXIT_SUCCESS;
