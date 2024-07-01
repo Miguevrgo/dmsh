e3928575d1acf6144abd00a0464a5efccdc75d56
miguevrgo <miguevrgo@gmail.com>
2024-07-01 19:57:51 +0200

    Fix qcat formatting, Upload test file, Fix redirect output

--- builtin/qcat/src/main.rs
+++ builtin/qcat/src/main.rs
@@ -1,6 +1,9 @@
+use core::ffi::c_int;              /// TODO:Choose between this line 
+use libc::{isatty, STDOUT_FILENO}; ///      and this one (see fn is_terminal)
 use std::env;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
+use term_size::dimensions;
 use text_colorizer::*;
 
 fn print_usage() {
@@ -11,34 +14,59 @@ fn print_usage() {
     eprintln!("Usage: qcat [OPTION]... [FILE]...");
 }
 
-/// TODO: Change this behaviour, uploaded for contributors to know the idea
-/// behind, missing adjusting numbers and lines, maybe changing filename to exclude
-/// route
-fn print_boxed_content(file_name: &str, lines: Vec<String>) {
-    let max_line_width = lines.iter().map(|line| line.len()).max().unwrap_or(0);
-    let width = max_line_width + 4;
-    let max_width = width;
+fn print_boxed_content(file_name: &str, lines: &[String]) {
+    let term_width: usize = dimensions().map(|(w, _)| w).unwrap_or(80);
 
-    println!("┏{:━<width$}┓", "", width = width);
-    println!("┃ {: ^width$} ┃", file_name.green(), width = width - 2);
-    println!("┗{:━<width$}┛", "", width = width);
+    println!(
+        "\u{250F}{:\u{2501}<width$}\u{2513}",
+        "",
+        width = term_width - 15,
+    );
+    println!(
+        "\u{2503} {: ^width$} \u{2503}",
+        file_name.green(),
+        width = term_width - 17,
+    );
+    println!(
+        "\u{2517}{:\u{2501}<width$}\u{251B}",
+        "",
+        width = term_width - 15
+    );
 
     for (index, line) in lines.iter().enumerate() {
         println!(
-            "┃ {: >4} ┃ {: ^max_width$} ┃",
-            index + 1,
+            "\u{2503} {} \u{2503} {}",
+            format!("{: >4}", index + 1),
             line,
-            max_width = max_width,
         );
     }
 
-    println!("┗{:━<width$}┛", "", width = width);
+    println!(
+        "\u{2517}{:\u{2501}<width$}\u{2501}",
+        "",
+        width = term_width - 10,
+    );
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
+fn print_plain_content(lines: &[String]) {
+    println!("{}", lines.join("\n"));
+}
 
-    if args.len() < 2 {
+fn is_terminal() -> bool {
+    unsafe { isatty(STDOUT_FILENO) != 0 }
+}
+
+/// fn is_terminal() -> bool {
+///     extern "C" {
+///         pub fn isatty(fd: c_int) -> c_int;
+///     }
+///     unsafe { isatty(STDOUT_FILENO) != 0 }
+/// }
+
+fn main() {
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() < 1 {
         print_usage();
         eprintln!(
             "{} wrong number of arguments: expected > 1, got {}.",
@@ -48,7 +76,7 @@ fn main() {
         std::process::exit(1);
     }
 
-    for file_name in &args[1..] {
+    for file_name in &args[0..] {
         match File::open(file_name) {
             Ok(file) => {
                 let reader = BufReader::new(file);
@@ -56,7 +84,11 @@ fn main() {
                     .lines()
                     .map(|l| l.unwrap_or_else(|_| String::new()))
                     .collect();
-                print_boxed_content(&file_name, lines);
+                if is_terminal() {
+                    print_boxed_content(&file_name, &lines);
+                } else {
+                    print_plain_content(&lines);
+                }
             }
             Err(e) => {
                 eprintln!("Error opening {}: {}", file_name, e);
